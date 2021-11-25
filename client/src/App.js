4159d7c51f4bfc03f6194da87b3f2bfce01ad768
rutvik11062000 <rutvik.chandla@gmail..com>
2021-11-25 17:28:50 +0530

    Integrated private route and session storage

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,33 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
 import Login from "./pages/login";
+import Main from "./pages/main";
 import Register from "./pages/register";
 import Welcome from "./pages/Welcome";
-
+import { AuthRoute, PrivateRoute } from "./util/AuthRoute";
 
 function App() {
   return (
-    <BrowserRouter >
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter >
+    <AuthProvider>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<PrivateRoute />} >
+            <Route path="/" element={<Main />} />
+          </Route>
+          <Route path="/welcome" element={<AuthRoute />} >
+            <Route path="/welcome" element={<Welcome />} />
+          </Route>
+          <Route path="/login" element={<AuthRoute />} >
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route path="/register" element={<AuthRoute />} >
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter >
+    </AuthProvider>
   );
 }
 
--- client/src/util/AuthRoute.js
+++ client/src/util/AuthRoute.js
@@ -1,19 +1,22 @@
 import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 import { AuthContext } from '../context/auth';
 
-function AuthRoute({ component: Component, ...rest }) {
+function AuthRoute({ element: Component, ...rest }) {
   const { user } = useContext(AuthContext);
 
   return (
-    <Route
-      {...rest}
-      render={(props) =>
-        user ? <Redirect to="/" /> : <Component {...props} />
-      }
-    />
-  );
+    user ? <Navigate to="/" /> : <Outlet />
+  )
 }
 
-export default AuthRoute;
+function PrivateRoute({ element: Component, ...rest }) {
+  const { user } = useContext(AuthContext);
+
+  return (
+    user ? <Outlet /> : <Navigate to="/welcome" />
+  )
+}
+
+export { AuthRoute, PrivateRoute };
